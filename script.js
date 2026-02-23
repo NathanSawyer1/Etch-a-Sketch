9a1143d575ad0c0522d8fe167bccffe4540dbc88
Nathan <159974115+NathanSawyer1@users.noreply.github.com>
2026-02-23 01:31:00 -0800

    Now each time you hover over an element the color will change, also the opacity increases each consecutive time you hover over the same element.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const cnt = document.getElementById('container');
 
+function randomColor() {
+
+const red = Math.floor(Math.random() * 255);
+const green = Math.floor(Math.random() * 255);
+const blue = Math.floor(Math.random() * 255);
+
+return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+}
 
 document.getElementById('choice').addEventListener('click', (e) => {
     
@@ -21,15 +29,19 @@ for (let i = 0; i < choice*choice; i++) {
 const newDiv = document.createElement('div');
 newDiv.classList.add('element')
 newDiv.classList.add(`item${i}`);
-newDiv.style.backgroundColor = 'black';
+newDiv.dataset.opacity = 0;
 
 newDiv.addEventListener('mouseenter', (e) => {
-    e.target.style.backgroundColor = "gray";
+    e.target.style.backgroundColor = randomColor();
+    let currentOpacity = Number(e.target.dataset.opacity);
+    currentOpacity += 0.1;
+    e.target.style.opacity = currentOpacity;
+    newDiv.dataset.opacity = currentOpacity; 
 });
 
-newDiv.addEventListener('mouseleave', (e) => {
-    e.target.style.backgroundColor = "black";
-})
+// newDiv.addEventListener('mouseleave', (e) => {
+//     e.target.style.backgroundColor = "black";
+// })
 
 cnt.appendChild(newDiv);
 }
