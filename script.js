97e5ff3fff2805d13062bfc9bceffb404c6496a2
Nathan <159974115+NathanSawyer1@users.noreply.github.com>
2026-02-22 23:43:48 -0800

    hover feature is working, each element currently just turns to grey on hover and back to black after leaving.

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ const cnt = document.getElementById('container');
 
 for (let i = 0; i < 256; i++) {
 const newDiv = document.createElement('div');
-newDiv.classList.add("item");
+newDiv.classList.add('element')
+newDiv.classList.add(`item${i}`);
 newDiv.style.backgroundColor = 'black';
+
+newDiv.addEventListener('mouseenter', (e) => {
+    e.target.style.backgroundColor = "gray";
+});
+
+newDiv.addEventListener('mouseleave', (e) => {
+    e.target.style.backgroundColor = "black";
+})
+
 cnt.appendChild(newDiv);
 }
