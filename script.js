ac4b2b8561225620032b0476636730b94e40c6fd
Nathan <159974115+NathanSawyer1@users.noreply.github.com>
2026-02-23 00:51:31 -0800

    the button works and the grid is reset each time the user enters a new grid size.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const cnt = document.getElementById('container');
 
-for (let i = 0; i < 256; i++) {
+
+document.getElementById('choice').addEventListener('click', (e) => {
+    
+    let choice;
+
+    while (true) {
+        
+    choice = prompt('Select Grid Size:', 'max: 100');
+        if (Number(choice) >= 100) {
+        alert("Number too high! Try again!");
+        }   else if (Number(choice) < 100) {
+        break;
+        }
+    
+    }
+    cnt.innerHTML = "";
+
+for (let i = 0; i < choice*choice; i++) {
 const newDiv = document.createElement('div');
 newDiv.classList.add('element')
 newDiv.classList.add(`item${i}`);
@@ -16,3 +33,6 @@ newDiv.addEventListener('mouseleave', (e) => {
 
 cnt.appendChild(newDiv);
 }
+
+
+})
